35f8b1b346ab0de802fd4ae94ab7f4c4981ba92e
kolan51 <jasa1244@gmail.com>
2021-12-08 11:36:41 +0100

    24

--- models/src/main/resources/sql-scripts/init.sql
+++ models/src/main/resources/sql-scripts/init.sql
@@ -1,1 +1,1 @@
-INSERT INTO customers (id, firstName, lastName, streetAddress, postcode, birthDate) VALUES (1, 'Janko', 'Stanko', 'Vinkota Vodopivca 1', 5267, 1970-01-18);
+INSERT INTO customers (customerId, firstName, lastName, streetAddress, postcode, birthDate) VALUES (1, 'Janko', 'Stanko', 'Vinkota Vodopivca 1', 5267, 1970-01-18);
